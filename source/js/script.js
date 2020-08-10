f6ca44f7ee6aa93f1979fe42feda06f5a2e581cc
unknown <andrew2263@gmail.com>
2020-08-11 02:33:54 +0300

    layout commit 11-08-2020

---
+++ source/js/script.js
@@ -0,0 +1,53 @@
+'use strict';
+(function () {
+  var tabsLinks = document.querySelectorAll('.tabs__link');
+  var tabsItems = document.querySelectorAll('.tabs__item');
+  var anchorLinks = document.querySelectorAll('.anchor-link');
+  var ticketsBuyButtons = document.querySelectorAll('.tickets__buy');
+  var superButton = document.querySelector('.super__request');
+
+  for (var i = 0; i < tabsLinks.length; i++) {
+    tabsLinks[i].addEventListener('click', function (e) {
+      e.preventDefault();
+      var hrefElement = document.querySelector(e.target.getAttribute('href'));
+      var tabsPanels = document.querySelectorAll('.tabs__panel');
+      for (var j = 0; j < tabsPanels.length; j++) {
+        tabsPanels[j].classList.remove('tabs__panel_active');
+        tabsPanels[j].classList.remove('in');
+      }
+      hrefElement.classList.add('tabs__panel_active');
+      setTimeout(function () {
+        hrefElement.classList.add('in');
+      }, 150);
+      for (var k = 0; k < tabsItems.length; k++) {
+        tabsItems[k].classList.remove('tabs__item_active');
+      }
+      e.target.parentNode.classList.add('tabs__item_active');
+    });
+  }
+
+  for (var n = 0; n < anchorLinks.length; n++) {
+    anchorLinks[n].addEventListener('click', function (e) {
+      e.preventDefault();
+      window.scrollTo({
+        top: document.querySelector(this.getAttribute('href')).offsetTop,
+        left: 0,
+        behavior: 'smooth'
+      });
+    });
+  }
+
+  for (var m = 0; m < ticketsBuyButtons.length; m++) {
+    ticketsBuyButtons[m].addEventListener('click', function (e) {
+      e.preventDefault();
+      location.href = 'buy.html';
+    })
+  }
+
+  superButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    location.href = 'super.html';
+  });
+
+
+})();
